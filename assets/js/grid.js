ba7b71face04e2fd4f79aa6be792e42573fe731e
josejosepreso <josehihat@gmail.com>
2025-08-26 16:03:17 -0600

    Delete assets

--- assets/js/grid.js
+++
@@ -1,5 +0,0 @@
-const grid = document.querySelector("div#art-grid");
-for (let i = 0; i < N_DIVS * 2; ++i) {
-	const item = new GalleryItem("AAA", "assets/img/01.jpg", "#");
-	grid.appendChild(item);
-}
